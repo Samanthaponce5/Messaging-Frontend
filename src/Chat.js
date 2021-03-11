e070a779f7b1a8518fa46f7f46ece834233683de
Samanthaponce5 <samanthaponce5@gmail.com>
2021-03-11 18:17:55 -0500

    chat design

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,56 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import SearchOutlined from '@material-ui/icons/SearchOutlined'
+import AttachFile from '@material-ui/icons/AttachFile'
+import MoreVert from '@material-ui/icons/MoreVert'
+
 import React from 'react'
 import './Chat.css'
 
 function Chat(){
 
     return <div className='chat'>
-        <h1>This is the Chat</h1>
+        <div className='chat__header'>
+        <Avatar/>
+        <div className='chat__headerInfo'>
+            <h3>Room name</h3>
+            <p>Last seen at...</p>
+        </div>
+        <div className='chat__headerRight'>
+            <IconButton>
+                <SearchOutlined/>
+            </IconButton>
+
+            <IconButton>
+                <AttachFile/>
+            </IconButton>
+
+            <IconButton>
+                <MoreVert/>
+            </IconButton>
+        </div>
+        </div>
+
+        <div className='chat__body'>
+            <p className='chat__message'>
+                <span className='chat__name'>Sam</span>
+                This is a msg
+                <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+            </p>
+
+
+            <p className='chat__message chat__reciever'>
+                <span className='chat__name'>Sam</span>
+                This is a msg
+                <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+            </p>
+
+
+            <p className='chat__message'>
+                <span className='chat__name'>Sam</span>
+                This is a msg
+                <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+            </p>
+        </div>
     </div>
 }
 
